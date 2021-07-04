7c85da86fa674ccbd45c72b13af552acf832cb0d
FBerman96 <fernando.faour@davinci.edu.ar>
2021-07-04 00:29:54 -0300

    Giphy Update

--- src/gifComponents/Gifs.js
+++ src/gifComponents/Gifs.js
@@ -5,7 +5,6 @@ import './Gifs.css'
 
 export default function Gifs ({title, url, sGifs}){
   return (
-        
         <a href={`#${title}`} className='Gifs'>
           <h4>{title}</h4>
           <small></small>
